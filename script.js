d277f9c98ef07a315a5154e283bd223c3062b13f
MollyZalman <68440011+MollyZalman@users.noreply.github.com>
2020-07-21 17:16:08 -0400

    Try 1

--- script.js
+++ script.js
@@ -97,8 +97,13 @@ function getRandomQuote () {
  * `printQuote` function
 ***/
 function printQuote () {
-  const quote = getRandomQuote()
-
+  const quote = getRandomQuote();
+  if (condition1)
+  statement1
+else if (condition2)
+  statement2
+else if (condition3)
+  statement3
 }
 
 /***
